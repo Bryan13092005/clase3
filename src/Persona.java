415c044b6a8ace485262b9ab54faa9a9357bd687
POO <bryan.salvador@epn.edu.ec>
2025-10-06 11:13:04 -0500

    Clase persona

---
+++ src/Persona.java
@@ -0,0 +1,13 @@
+public class Persona{
+    int cedula;
+    String nombre;
+    int edad;
+    String correo;
+    public Persona(int cedula,String nombre,int edad, String correo){
+        this.cedula=cedula;
+        this.nombre=nombre;
+        this.edad=edad;
+        this.correo=correo;
+    }
+}
+
