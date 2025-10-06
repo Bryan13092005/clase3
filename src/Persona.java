c6c5b3e5523c3762d0ee14c8c4a02a1fcd4cafae
POO <bryan.salvador@epn.edu.ec>
2025-10-06 11:29:18 -0500

    Clase persona con 2 objetos

--- src/Persona.java
+++ src/Persona.java
@@ -9,5 +9,17 @@ public class Persona{
         this.edad=edad;
         this.correo=correo;
     }
+
+    void imprimir(){
+        System.out.println("Bienvenido"+" "+nombre+" "+cedula+" "+edad+" "+correo);
+    }
+
+    public static void main(String[] args) {
+        Persona p1=new Persona(1753655172,"Bryan Salvador",20,"bryanrsalvadormartinez@gmail.com");
+        p1.imprimir();
+        Persona p2=new Persona(1711684132,"Ximena",46,"ximena@gmail.com");
+        p2.imprimir();
+
+    }
 }
 
